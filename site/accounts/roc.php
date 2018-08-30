4883073c1706937ac6728d422b7cb09cc02f30da
Ride or Cry <jacob@rideorcry.com>
2018-08-30 12:42:27 -0700

    new artist folders and now thats what i call music

---
+++ site/accounts/roc.php
@@ -0,0 +1,14 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: roc
+password: >
+  $2a$10$OI83C2dIsul2Cotpw8wQZ.7/SyReY5TFBuYLlgP9m5NIzLyyIt5by
+email: jacob@rideorcry.com
+language: en
+role: admin
+history:
+  - desktop/ntwicf90s
+  - desktop/hip-hop
+  - desktop/tv
+  - desktop/albums/tom-petty-greatest-hits/sound-of-vinyl
+  - desktop/albums/tom-petty-greatest-hits
