6e960164f663a0138c1c191636da8debd55d054f
Ride or Cry <jacob@rideorcry.com>
2018-08-31 01:52:10 -0700

    ‘reload’

---
+++ site/accounts/roc.php
@@ -0,0 +1,15 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: roc
+password: >
+  $2a$10$OI83C2dIsul2Cotpw8wQZ.7/SyReY5TFBuYLlgP9m5NIzLyyIt5by
+email: jacob@rideorcry.com
+language: en
+role: admin
+history:
+  - >
+    desktop/albums/u2-the-joshua-tree-remastered/sound-of-vinyl
+  - desktop/albums/u2-the-joshua-tree-remastered/apple-video-playlist
+  - desktop/albums/u2-the-joshua-tree-remastered/apple-music
+  - desktop/albums/tom-petty-greatest-hits/sound-of-vinyl
+  - desktop/albums/tom-petty-greatest-hits/apple-video-playlist
